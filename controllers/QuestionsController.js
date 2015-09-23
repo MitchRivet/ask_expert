383256d7e99057d35634ba0f486b23fd8c990b8c
mitch rivet & ben casalino <student@epicodus.com>
2015-09-23 16:26:08 -0700

    filter answer attempt

--- controllers/QuestionsController.js
+++ controllers/QuestionsController.js
@@ -1,4 +1,8 @@
 askExpert.controller('QuestionsCtrl', function QuestionsCtrl($scope, QuestionsFactory) {
   $scope.questions = QuestionsFactory.questions;
   $scope.QuestionsFactory = QuestionsFactory;
-}); 
+
+  $scope.filterFn = function(question) {
+    return  question.answers && question.answers.length === 0;
+  };
+});
--- services/QuestionsFactory.js
+++ services/QuestionsFactory.js
@@ -7,6 +7,10 @@ askExpert.factory('QuestionsFactory', function QuestionsFactory() {
     factory.questionString = null;
   };
 
+  factory.filterFn = function(question) {
+    return  question.answers && question.answers.length === 0;
+  };
+
   return factory;
 
-}); 
+});
