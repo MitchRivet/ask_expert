7fa8210d3cd84b70fb87f53bee1e7a3a45ae00a1
Mitch Rivet & Ben Casalino <student@epicodus.com>
2015-09-24 13:33:38 -0700

    pre lunch update

--- controllers/AnswerController.js
+++ controllers/AnswerController.js
@@ -4,15 +4,23 @@ askExpert.controller('AnswerCtrl', function AnswerCtrl($scope, $stateParams, Que
 
 
   $scope.addAnswer = function() {
-    $scope.question.answers.push({ answer: $scope.questionAnswer, id: $scope.question.answers.length,  approved: false });
+    $scope.question.answers.push({ answer: $scope.questionAnswer, id: $scope.question.answers.length,  approved: false, votes: 0});
     $scope.questionAnswer = null;
 
   }
 
-  $scope.approveAnswer = function(answer, id) {
+  $scope.approveAnswer = function(answer, id, votes) {
 
-    $scope.question.answers[id] = ({ answer: answer, id: id, approved: true });
+    $scope.question.answers[id] = ({ answer: answer, id: id, approved: true, votes: votes});
 
+  }
+
+  $scope.upVote = function (answer, id, approved, votes) {
+      $scope.question.answers[id] = ({ answer: answer, id: id, approved: approved, votes: (votes + 1)})
+  }
+
+  $scope.downVote = function (answer, id, approved, votes) {
+    $scope.question.answers[id] = ({ answer: answer, id: id, approved: approved, votes: (votes - 1)})
   }
 
 
