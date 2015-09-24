36edb0b21327a9f0d7cad95da567badabd4dfe7a
Mitch Rivet & Ben Casalino <student@epicodus.com>
2015-09-24 14:54:37 -0700

    add wells and question display

--- controllers/QuestionsController.js
+++ controllers/QuestionsController.js
@@ -8,4 +8,8 @@ askExpert.controller('QuestionsCtrl', function QuestionsCtrl($scope, QuestionsFa
     return  question.answers && question.answers.length === 0;
   };
 
+  $scope.filterFn2 = function(question) {
+    return  question.answers && question.answers.length != 0;
+  };
+
 });
